c73a1d8e94dabfabfc1a4a42d94a971942351e0d
Vincent Lin <vincent.open.source.taiwan@gmail.com>
2024-02-13 16:26:59 +0800

    setup modal

--- lib/modal_web/live/home.ex
+++ lib/modal_web/live/home.ex
@@ -4,10 +4,18 @@ defmodule ModalWeb.Home do
   def render(assigns) do
     ~H"""
     <h1>Welcome to Phoenix!</h1>
+    <.modal :if={@show_modal} id="modal" show on_cancel={JS.navigate(~p"/home")}>
+      this is a modal
+    </.modal>
+    <.button phx-click="show_modal">Show Modal</.button>
     """
   end
 
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    {:ok, assign(socket, show_modal: false)}
+  end
+
+  def handle_event("show_modal", _, socket) do
+    {:noreply, assign(socket, show_modal: true)}
   end
 end
